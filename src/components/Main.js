b9d0959805586b6d14ab56ed8fb9d94b834baa0a
Kirsty Alexandra Nguegang <kirstyalexandra.nguegang471@myci.csuci.edu>
2022-03-04 08:08:29 -0800

    Setting up the repo

---
+++ src/components/Main.js
@@ -0,0 +1,14 @@
+import { } from 'react-bootstrap';
+import React, { Component } from 'react'; 
+
+export default class Main extends Component
+{
+    render()
+    {
+        return (
+            <div>
+                <h2>Template to get started! :)</h2>
+            </div>
+        )
+    }
+}
